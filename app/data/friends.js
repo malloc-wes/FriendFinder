83eaa8269b04d06e62a68a2fc901be3541aca8d8
Wes A <39351248+malloc-wes@users.noreply.github.com>
2018-08-21 23:48:01 -0400

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+    {
+        name: "Rachel",
+        photo: "https://img.maximummedia.ie/her_ie/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvbWVkaWEtaGVyLm1heGltdW1tZWRpYS5pZS5zMy5hbWF6b25hd3MuY29tXFxcL3dwLWNvbnRlbnRcXFwvdXBsb2Fkc1xcXC8yMDE3XFxcLzA4XFxcLzI0MTMwMTM1XFxcL1NjcmVlbi1TaG90LTIwMTctMDgtMjQtYXQtMTMuMDAuNTktMTAyNHg1ODkucG5nXCIsXCJ3aWR0aFwiOjc0MCxcImhlaWdodFwiOjQxNixcImRlZmF1bHRcIjpcImh0dHBzOlxcXC9cXFwvd3d3Lmhlci5pZVxcXC9hc3NldHNcXFwvaW1hZ2VzXFxcL2hlclxcXC9uby1pbWFnZS5wbmc_dj01XCJ9IiwiaGFzaCI6IjUxZWQxYTU1ZDdmNDNhYjA0ZmU4OWRhMTE1YzIzZmNlNWNlMDI3NTUifQ==/screen-shot-2017-08-24-at-13-00-59-1024x589.png",
+scores: [
+            "5",
+            "2",
+            "1",
+            "4",
+            "2",
+            "3",
+            "1",
+            "3",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Phoebe",
+        photo: "http://beta.ems.ladbiblegroup.com/s3/content/808x455/03915b965a5676bace507967a1aa859b.png",
+scores: [
+            "4",
+            "3",
+            "1",
+            "5",
+            "5",
+            "5",
+            "3",
+            "4",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Chandler",
+        photo: "https://cdn-images-1.medium.com/max/1600/1*MOLjvPFGqbbZ_5qYlr11BA.png",
+scores: [
+            "1",
+            "2",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "3",
+            "5"
+        ]
+    },
+
+    {
+        name: "Ross",
+        photo: "https://i.amz.mshcdn.com/rmoDpLOnm_bDdz9TKsP0-vmhYWw=/950x534/filters:quality(90)/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F772918%2Fbe2e23d6-ec01-406b-80d5-9c0cbe37aeb7.jpg",
+scores: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+
+    {
+        name: "Monica",
+        photo: "https://imgix.bustle.com/rehost/2016/9/13/04a388e1-335b-4fad-969f-383866bddab7.png?w=970&h=582&fit=crop&crop=faces&auto=format&q=70",
+scores: [
+            "1",
+            "1",
+            "1",
+            "3",
+            "3",
+            "3",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Joey",
+        photo: "https://img.maximummedia.ie/joe_ie/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvbWVkaWEtam9lLm1heGltdW1tZWRpYS5pZS5zMy5hbWF6b25hd3MuY29tXFxcL3dwLWNvbnRlbnRcXFwvdXBsb2Fkc1xcXC8yMDE1XFxcLzEwXFxcLzI5MTExMzEyXFxcL0pvZXkuanBnXCIsXCJ3aWR0aFwiOjc2NyxcImhlaWdodFwiOjQzMSxcImRlZmF1bHRcIjpcImh0dHBzOlxcXC9cXFwvd3d3LmpvZS5pZVxcXC9hc3NldHNcXFwvaW1hZ2VzXFxcL2pvZVxcXC9uby1pbWFnZS5wbmc_dj01XCJ9IiwiaGFzaCI6IjQ5OGEyZTllYjQ3NWYyMDczMzE5NTg3ZjQ5OWZhMjYxZjg0YzNlZDYifQ==/joey.jpg",
+scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    }
+];
+
+module.exports = friends;
